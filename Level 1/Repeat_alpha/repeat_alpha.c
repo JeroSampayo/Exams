59ca3ccfec0cb95084c6408ff702e18878c0c531
Jeronimo Miras sampyo <jmiras-s@cbr10s6.42barcelona.com>
2023-05-23 17:00:30 +0200

    Sort

---
+++ Level 1/Repeat_alpha/repeat_alpha.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   repeat_alpha.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 14:00:24 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/05/23 16:59:08 by jmiras-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+void    ft_putchar_n(char c, int i)
+{
+    while (i > 0)
+    {
+        write(1, &c, 1);
+        --i;
+    }
+}
+
+void	repeat(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str >= 'a' && *str <= 'z')
+			ft_putchar(*str, *str + 1 - 'a');
+		else if (*str >= 'A' && *str <= 'Z')
+			ft_putchar(*str, *str + 1 - 'A');
+		else
+			write(1, str, 1);
+		str++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		repeat(argv[1]);
+	
+	write(1, "\n", 1);
+    return (0);
+}
