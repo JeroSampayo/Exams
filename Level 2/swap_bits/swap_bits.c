5ddb4ae33c3468ce6e0841b4e38d68be37ea880d
Jeronimo Miras sampyo <jmiras-s@cbr5s5.42barcelona.com>
2023-05-25 17:25:38 +0200

    Sorting

--- Level 2/swap_bits/swap_bits.c
+++ Level 2/swap_bits/swap_bits.c
@@ -1,40 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bits2.c                                            :+:      :+:    :+:   */
+/*   swap_bits.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/24 16:06:39 by jmiras-s          #+#    #+#             */
-/*   Updated: 2023/03/24 17:28:58 by jmiras-s         ###   ########.fr       */
+/*   Created: 2023/05/25 17:10:52 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/05/25 17:11:06 by jmiras-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
 
-int	reverse_bits(unsigned int num)
+unsigned char	swap_bits(unsigned char octet)
 {
-	unsigned int max_pos;
-	unsigned int rev_num;
-	int i;
-	rev_num = 0;
-	max_pos = 1 << 31;
-	i = 1;
-	while (max_pos >= 1)
-	{
-		if (num & i)
-		{
-			rev_num = max_pos | rev_num;
-		}
-		i = i << 1;
-		max_pos = max_pos >> 1;
-	}
-	printf("%d", rev_num);
-	return (rev_num);
-}
+	int temp;
 
-int main(void)
-{
-	int i = 0;
-	i = reverse_bits(88);
-	printf("%d", i);
+	temp = octet & 0x0F;
+	octet = octet >> 4;
+	temp = temp << 4;
+	octet = octet | temp;
+
+	return (octet);
 }
