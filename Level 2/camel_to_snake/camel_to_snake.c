89b1df02258608f118901479b52c5a0873abadf0
Jeronimo Miras sampyo <jmiras-s@cbr13s4.42barcelona.com>
2023-05-30 18:42:39 +0200

    Bonus

--- Level 2/camel_to_snake/camel_to_snake.c
+++ Level 2/camel_to_snake/camel_to_snake.c
@@ -1,30 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   alpha_mirror.c                                     :+:      :+:    :+:   */
+/*   camel_to_snake.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/30 16:11:40 by jmiras-s          #+#    #+#             */
-/*   Updated: 2023/05/30 17:29:51 by jmiras-s         ###   ########.fr       */
+/*   Created: 2023/05/30 18:24:54 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/05/30 18:42:12 by jmiras-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 int main(int argc, char **argv)
 {
 	int i = 0;
 	if (argc == 2)
 	{
-		while(argv[1][i])
+		while (argv[1][i])
 		{
 			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				argv[1][i] = 'Z' -  argv[1][i] + 'A';
-			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-				argv[1][i] = 'z' - argv[1][i] + 'a';
+			{
+				argv[1][i] = argv[1][i] + 32;
+				write(1, "_", 1);
+			}
 			write(1, &argv[1][i], 1);
 			i++;
 		}
 	}
-	write(1, "\n", 1);
+	write (1, "\n", 1);
 }
