a8d8b2ef2fc6fe91812571095e9316f11833ecaa
Jeronimo Miras sampyo <jmiras-s@cbr6s4.42barcelona.com>
2023-06-05 11:34:26 +0200

    Strcmp

---
+++ Level 2/wdmatch/wdmatch.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wdmatch.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/03 12:35:19 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/06/03 14:22:32 by jmiras-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+void ft_putstr(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int i2 = 0;
+
+	if (argc == 3)
+	{
+		while (argv[2][i2])
+			if (argv[2][i2] == argv[1][i]) 
+			{
+				i2++;
+				i += 1;
+			}	
+		if(!argv[1][i])
+			ft_putstr(argv[1]);
+	}
+	write(1, "\n", 1);
+	return(0);
+}
+
