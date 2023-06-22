36cf8aa0e6b1aeafc85e3f4ee9093817b382e0db
JeronimoMiras <113709509+JeroSampayo@users.noreply.github.com>
2023-06-22 11:03:47 +0200

    Update repeat_alpha.c

--- Level 1/Repeat_alpha/repeat_alpha.c
+++ Level 1/Repeat_alpha/repeat_alpha.c
@@ -5,40 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/23 14:00:24 by jmiras-s          #+#    #+#             */
-/*   Updated: 2023/05/23 17:16:58 by jmiras-s         ###   ########.fr       */
+/*   Created: 2023/06/22 10:46:58 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/06/22 11:02:28 by jmiras-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-void    ft_putchar(char c, int i)
-{
-    while (i > 0)
-    {
-        write(1, &c, 1);
-        --i;
-    }
-}
-
-void	repeat(char *str)
-{
-	while (*str != '\0')
-	{
-		if (*str >= 'a' && *str <= 'z')
-			ft_putchar(*str, *str + 1 - 'a');
-		else if (*str >= 'A' && *str <= 'Z')
-			ft_putchar(*str, *str + 1 - 'A');
-		else
-			write(1, str, 1);
-		str++;
-	}
-}
-
 int main(int argc, char **argv)
 {
+	int i = -1;
+	int count = 0;
+
 	if (argc == 2)
-		repeat(argv[1]);
-	
+	{
+		while (argv[1][++i])
+		{
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				count = argv[1][i] - 96;
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				count = argv[1][i] - 64;
+			else
+				write(1, &argv[1][i], 1);
+			while (count)
+			{
+				write(1, &argv[1][i], 1);
+				count--;
+			}
+		}
+	}
 	write(1, "\n", 1);
     return (0);
 }
