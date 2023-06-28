c05c63bedb1db029fc1650fdcad8cd726483b49e
JeronimoMiras <113709509+JeroSampayo@users.noreply.github.com>
2023-06-28 17:41:10 +0200

    Update union.c

--- Level 2/union/union.c
+++ Level 2/union/union.c
@@ -5,47 +5,45 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/19 10:57:39 by jmiras-s          #+#    #+#             */
-/*   Updated: 2023/06/19 11:16:54 by jmiras-s         ###   ########.fr       */
+/*   Created: 2023/06/28 17:01:55 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/06/28 17:40:39 by jmiras-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-int check(int c, char *str, int index)
+#include <unistd.h>
+int check(int c, char *str, int k)
 {
 	int i = 0;
 
-	while(i < index)
+	while (i < k)
 	{
 		if (str[i] == c)
 			return 0;
 		i++;
 	}
-	return  1;
+	return 1;
 }
 
 int main(int argc, char **argv)
 {
-	int i;
-	int i2;
-	int i3;
-
+	int i = -1;
+	int j = -1;
+	int k = 0;
 	if (argc == 3)
 	{
-		while (argv[1][i] != '\0')
-			i++;
-		while (argv[2][i2] != '\0')
+		while (argv[1][++i])
+		while (argv[2][++j])
 		{
-			argv[1][i] = argv[2][i2];
-			i++;
-			i2++;
+			if (argv[1][i] == argv[2][j])
+			{
+				argv[1][i] = argv[2][j];
+				i++;
+			}
 		}
-		i--;
-		while (i3 <= i)
+		while (k < i)
 		{
-			if (check(argv[1][i3], argv[1], i3) == 1)
-				write (1, &argv[1][i3], 1);
-			i3++;
+			if (check(argv[1][k], argv[1], k) == 1)
+					write(1, &argv[1][k], 1);
+			k++;
 		}
 	}
 	write(1, "\n", 1);
