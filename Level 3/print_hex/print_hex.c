bfc624ae060b9c934f3be375868e4c7a08a9cdf6
Jeronimo Miras sampyo <jmiras-s@car13s5.42barcelona.com>
2023-07-25 14:07:10 +0200

    print_hex

---
+++ Level 3/print_hex/print_hex.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_hex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 13:50:10 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/07/25 14:01:23 by jmiras-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+int	ft_atoi(char *str)
+{
+	int n = 0;
+	
+	while(*str != '\0')
+	{
+		n *= 10;
+		n = n + *str - '0';
+		str++;
+	}	
+	return(n);
+}
+
+void	print_hex(int n)
+{
+	char hex_digits[] = "0123456789abcdef";
+
+	if (n >= 16)
+		print_hex(n / 16);
+	write(1, &hex_digits[n % 16], 1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		print_hex(ft_atoi(argv[1]));
+	write(1, "\n", 1);
+}
